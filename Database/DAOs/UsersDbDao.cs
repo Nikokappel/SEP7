3f9ebd7fae0d003a18f063acde3146ec23255b75
Nikokappel <nikokappel@hotmail.dk>
2023-11-06 12:57:27 +0100

    updt

--- Database/DAOs/UsersDbDao.cs
+++ Database/DAOs/UsersDbDao.cs
@@ -1,17 +1,6 @@
 ﻿namespace DefaultNamespace;
 
-public class UsersDbDao : DbContext
+public class UsersDbDao
 {
-    public DbSet<User> Users { get; set; }
-    public DbSet<Batch> Batch { get; set; }
-    public DbSet<DateTime> DateTime { get; set; }
-    public DbSet<Error> Error { get; set; }
-    public DbSet<Machine> Machine { get; set; }
-    public DbSet<Types> Types { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlite("Data Source = Todo.db");
-        optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);            
-    }
+    
 }
