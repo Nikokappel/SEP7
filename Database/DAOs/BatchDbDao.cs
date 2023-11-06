af8218226255095c2b861aad2203421f5c8cd9e4
Nikokappel <nikokappel@hotmail.dk>
2023-11-06 12:27:03 +0100

    Update

---
+++ Database/DAOs/BatchDbDao.cs
@@ -0,0 +1,33 @@
+﻿namespace DefaultNamespace;
+
+public class BatchDbDao
+{
+    /*public class BatchDbDao : IBatchDao
+{
+  public Task<Todo> CreateAsync(Todo todo)
+  {
+      throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Todo>> GetAsync(SearchTodoParametersDto searchParameters)
+  {
+      throw new NotImplementedException();
+  }
+
+  public Task UpdateAsync(Todo todo)
+  {
+      throw new NotImplementedException();
+  }
+
+  public Task<Todo?> GetByIdAsync(int todoId)
+  {
+      throw new NotImplementedException();
+  }
+
+  public Task DeleteAsync(int id)
+  {
+      throw new NotImplementedException();
+  }
+}
+*/
+}
